d666736ba58a1980f3f05645c3ad651f5bdc403d
Anton Kashirov <anton.kashirov@gmail.com>
2025-05-22 17:31:35 +0300

    fix tests

--- src/endpoint-microservice/shared/schema/plugins/__tests__/row-id.spec.ts
+++ src/endpoint-microservice/shared/schema/plugins/__tests__/row-id.spec.ts
@@ -1,13 +1,13 @@
 import Ajv from 'ajv/dist/2020';
 import addFormats from 'ajv-formats';
-import { rowHashSchema } from 'src/endpoint-microservice/shared/schema';
+import { rowIdSchema } from 'src/endpoint-microservice/shared/schema';
 
 describe('row-id-schema', () => {
   const ajv = new Ajv();
   addFormats(ajv);
 
   it('base tests', () => {
-    expect(ajv.validate(rowHashSchema, '')).toBe(true);
-    expect(ajv.validate(rowHashSchema, 1)).toBe(false);
+    expect(ajv.validate(rowIdSchema, '')).toBe(true);
+    expect(ajv.validate(rowIdSchema, 1)).toBe(false);
   });
 });
