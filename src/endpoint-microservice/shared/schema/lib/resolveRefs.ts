458e1c94a054c7e61a8a321a9eaae1614e959d41
Anton Kashirov <anton.kashirov@gmail.com>
2025-05-22 10:42:10 +0300

    feat: system fields schemas

--- src/endpoint-microservice/shared/schema/lib/resolveRefs.ts
+++ src/endpoint-microservice/shared/schema/lib/resolveRefs.ts
@@ -1,11 +1,25 @@
 import { SystemSchemaIds } from 'src/endpoint-microservice/shared/schema-ids.consts';
 import { createJsonSchemaStore } from 'src/endpoint-microservice/shared/schema/lib/createJsonSchemaStore';
-import { fileSchema } from 'src/endpoint-microservice/shared/schema/plugins';
+import {
+  fileSchema,
+  rowCreatedAtSchema,
+  rowCreatedIdSchema,
+  rowHashSchema,
+  rowSchemaHashSchema,
+  rowUpdatedAtSchema,
+  rowVersionIdSchema,
+} from 'src/endpoint-microservice/shared/schema/plugins';
 import { rowIdSchema } from 'src/endpoint-microservice/shared/schema/plugins/row-id.schema';
 import { JsonSchema } from 'src/endpoint-microservice/shared/schema/types';
 
 export const pluginRefs: Readonly<Record<string, JsonSchema>> = {
   [SystemSchemaIds.RowId]: rowIdSchema,
+  [SystemSchemaIds.RowVersionId]: rowVersionIdSchema,
+  [SystemSchemaIds.RowCreatedId]: rowCreatedIdSchema,
+  [SystemSchemaIds.RowCreatedAt]: rowCreatedAtSchema,
+  [SystemSchemaIds.RowUpdatedAt]: rowUpdatedAtSchema,
+  [SystemSchemaIds.RowHash]: rowHashSchema,
+  [SystemSchemaIds.RowSchemaHash]: rowSchemaHashSchema,
   [SystemSchemaIds.File]: fileSchema,
 };
 
