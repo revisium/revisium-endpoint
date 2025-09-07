25100c2c2d8e0ed98f5062f293958d0bb464a6b9
Anton Kashirov <anton.kashirov@gmail.com>
2025-09-07 12:30:33 +0300

    feat: db polling

--- src/endpoint-microservice/synchronization/synchronization.module.ts
+++ src/endpoint-microservice/synchronization/synchronization.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
+import { DatabaseModule } from 'src/endpoint-microservice/database/database.module';
 import { EndpointSyncManager } from './endpoint-sync-manager.service';
+import { DbPollingStrategy } from './strategies/db-polling.strategy';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, ConfigModule, DatabaseModule],
   providers: [
     EndpointSyncManager,
+    DbPollingStrategy,
     {
       provide: 'SYNC_STRATEGIES',
-      useFactory: () => [],
-      inject: [],
+      useFactory: (dbPolling: DbPollingStrategy) => [dbPolling],
+      inject: [DbPollingStrategy],
     },
   ],
-  exports: [EndpointSyncManager],
+  exports: [EndpointSyncManager, DbPollingStrategy],
 })
 export class SynchronizationModule {}
